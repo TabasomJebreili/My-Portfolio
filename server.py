25e545f2fb5d908f0b949ba429894f270d3ec636
Tabasom <tabasom.jebreili@gmail.com>
2025-09-30 15:33:05 +0330

    Initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def welcome():
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
