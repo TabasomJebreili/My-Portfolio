fe6d8de2a714ab327ce6e020dd1e9747e9f6514a
Tabasom <tabasom.jebreili@gmail.com>
2025-10-05 18:13:59 +0330

    secure form and receive form info in an email

--- server.py
+++ server.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+import os
+from dotenv import load_dotenv
+import smtplib
+
+
+class Form(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    subject = StringField('subject', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), Email()])
+    message = StringField('message', validators=[DataRequired(), Length(max=500)])
+
 
 app = Flask(__name__)
 
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    contact_form = Form()
+    if contact_form.validate_on_submit():
+        name = contact_form.name.data
+        email = contact_form.email.data
+        subject = contact_form.subject.data
+        message = contact_form.message.data
+        user = os.getenv("FROM_EMAIL")
+        password = os.getenv("FROM_EMAIL_PASSWORD")
+        to_adr = os.getenv("TO_EMAIL")
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=user, password=password)
+            connection.sendmail(from_addr=user, to_addrs=to_adr, msg=f"Subject:{subject}\n\n{message}\n"
+                                                                     f"This massage is from user:{name} with email of:"
+                                                                     f"{email}")
+        return redirect(url_for('home') + '#contact')
+
+    else:
+        return render_template('index.html', form=contact_form)
 
 
 @app.route('/starter')
